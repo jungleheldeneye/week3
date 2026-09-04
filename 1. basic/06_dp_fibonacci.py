ca9e60c180cadf3162621f4c753adddea14d6447
Heldeneye <Heldeneye@gmail.com>
2026-09-04 11:42:21 +0900

    06_answer

--- 1. basic/06_dp_fibonacci.py
+++ 1. basic/06_dp_fibonacci.py
@@ -83,13 +83,32 @@ def fibonacci_memo(n, memo=None):
     """
     # TODO: memo가 None이면 빈 딕셔너리로 초기화
     pass
-    
+
+
+    if memo == None:
+        memo = {}
+
+
     # TODO: base case 
     pass
+
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
     
     # TODO: 이미 계산한 값이 memo에 있으면 반환
     pass
-    
+
+
+    if n in memo:
+        return memo[n]
+    else:
+        memo[n] = fibonacci_memo(n-1,memo) + fibonacci_memo(n-2,memo)
+
+
+
+
     # TODO: 재귀 호출하여 계산하고 memo에 저장
     pass
     
